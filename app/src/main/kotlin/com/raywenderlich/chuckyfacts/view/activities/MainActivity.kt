b00792499f011de9fde855dac185321e5357c64c
MHO <mauricio.hdez.o.10@gmail.com>
2018-12-07 12:46:07 -0600

    View

--- app/src/main/kotlin/com/raywenderlich/chuckyfacts/view/activities/MainActivity.kt
+++ app/src/main/kotlin/com/raywenderlich/chuckyfacts/view/activities/MainActivity.kt
@@ -24,12 +24,63 @@ package com.raywenderlich.chuckyfacts.view.activities
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.support.v7.widget.Toolbar
+import android.view.View
+import android.widget.ProgressBar
+import com.raywenderlich.chuckyfacts.MainContract
 import com.raywenderlich.chuckyfacts.R
+import com.raywenderlich.chuckyfacts.entity.Joke
+import com.raywenderlich.chuckyfacts.view.adapters.JokesListAdapter
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.toolbar_view_custom_layout.*
+import org.jetbrains.anko.toast
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity(), MainContract.View {
+
+  private var presenter: MainContract.Presenter? = null
+  private val toolbar: Toolbar by lazy { toolbar_toolbar_view }
+  private val recyclerView: RecyclerView by lazy { rv_jokes_list_activity_main }
+  private val progressBar: ProgressBar by lazy { prog_bar_loading_jokes_activity_main }
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
+
+    presenter = MainPresenter(this)
+    recyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+    recyclerView.adapter = JokesListAdapter({ joke -> presenter?.listItemClicked(joke) }, null)
+  }
+
+  override fun onResume() {
+    super.onResume()
+    presenter?.onViewCreated()
+  }
+
+  override fun onDestroy() {
+    presenter?.onDestroy()
+    presenter = null
+    super.onDestroy()
+  }
+
+  override fun getToolbarInstance(): Toolbar? = toolbar
+
+  override fun showLoading() {
+    recyclerView.isEnabled = false
+    progressBar.visibility = View.VISIBLE
+  }
+
+  override fun hideLoading() {
+    recyclerView.isEnabled = true
+    progressBar.visibility = View.GONE
+  }
+
+  override fun publishDataList(data: List<Joke>) {
+    (recyclerView.adapter as JokesListAdapter).updateData(data)
+  }
+
+  override fun showInfoMessage(msg: String) {
+    toast(msg)
   }
 }
